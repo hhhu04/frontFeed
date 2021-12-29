24339d98b1e24a61ff4f636d389606349d3e35f7
Shin HY <hhhu04@gmail.com>
2021-12-29 14:52:32 +0900

    .

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+    <h1>home</h1>
+    <input type="text" v-model="title" />
+    <button type="button" @click="getData">get</button>
+    <button type="button" @click="setData">set</button>
+</div>
+</template>
+<script>
+export default {
+    data(){
+        return  { 
+            title: "개발자"
+        };
+    },
+    methods:{
+        getData() {
+            alert(this.title);
+        },
+        setData() {
+            this.title="12345"
+        }
+    }
+}
+</script>
